6cd4cde0fdf17ea4817bd15dac4c21c7961682e4
Samuel S. Furtado <salessamuel268@gmail.com>
2023-10-01 13:27:04 -0300

    feat: add Music class

---
+++ src/Domain/Music.java
@@ -0,0 +1,36 @@
+package Domain;
+
+public class Music extends Audio {
+
+    // PROPERTIES
+
+    String album;
+    String artist;
+    String gender;
+
+    // GET AND SET
+    
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
